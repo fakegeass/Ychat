cf8d2a8641a2c90de20845b56e7dcf28c11ec5a9
fakegeass <1013002673@qq.com>
2018-11-05 21:51:41 +0800

    daily update

--- client/client.go
+++ client/client.go
@@ -13,8 +13,12 @@ type client struct {
 }
 
 func main() {
-	serverAddr := os.Args[1]
+	serverAddr := "127.0.0.1"
+	if len(os.Args) == 2 {
+		serverAddr = os.Args[1]
+	}
 	conn, err := net.Dial("tcp", serverAddr+":8000")
+	defer conn.Close()
 	if err != nil {
 		fmt.Printf("Please check the address of the server(%v)\n", serverAddr)
 		os.Exit(-1)
@@ -24,24 +28,24 @@ func main() {
 	newClient := &client{userName, ""}
 	input := bufio.NewScanner(os.Stdin)
 	if input.Scan() {
-		newClient.userName = input.Text()+"\n"
-		fmt.Printf("Welcome  %v\n",newClient.userName)
+		newClient.userName = input.Text()
+		fmt.Printf("Welcome  %v!\n", newClient.userName)
 	}
-	_, err = fmt.Fprintf(conn, newClient.userName)
+	_, err = fmt.Fprintf(conn, newClient.userName+"\n")
 	if err != nil {
 		fmt.Printf("Name send error!\n")
 		os.Exit(2)
 	}
-	fmt.Println("Send name done!")
+	//fmt.Println("Send name done!")
 	inputMsg := bufio.NewScanner(os.Stdin)
-		for inputMsg.Scan() {
-			newClient.msg = inputMsg.Text()+"\n"
-			_, err = fmt.Fprintf(conn, newClient.msg)
+	for inputMsg.Scan() {
+		newClient.msg = inputMsg.Text() + "\n"
+		_, err = fmt.Fprintf(conn, newClient.msg)
 		if err != nil {
 			fmt.Printf("Send error! Message is:%v\n", newClient.msg)
 			os.Exit(2)
 		}
-		
+
 	}
 
 }
--- server/server.go
+++ server/server.go
@@ -1,51 +1,55 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
-	"bufio"
 )
 
-type client struct{
+type client struct {
 	userName string
 	userAddr string
-	msg string
-	online bool
+	msg      string
+	online   bool
 }
 
-func main(){
-	serverPort:=os.Args[1]
-	ln, err := net.Listen("tcp", ":"+serverPort)
-    if err != nil {
-    	fmt.Printf("占用端口失败！\n")
-    }
-    for {
-		conn, err := ln.Accept()
-		fmt.Printf("New client come in!\n")
-		if err != nil {
-    		fmt.Printf("%v！\n",err)
-    	}
-    	go handleConnection(conn)
-    }
-}
-
-func handleConnection(conn net.Conn){
-	var newClient client
-	newClient.userAddr=conn.RemoteAddr().String()
-	fmt.Printf("New client come in!Address is %v\n",newClient.userAddr)
-	
-	
-	inputMsg := bufio.NewScanner(conn)
-		for inputMsg.Scan() {
-			if newClient.online==false{
-				newClient.userName=inputMsg.Text()
-		fmt.Printf("%v enter the room\n",newClient.userName)
-				newClient.online=true
-			}else{
-				newClient.msg = inputMsg.Text()
-		fmt.Printf("%v:%v",newClient.userName,newClient.msg)
-			}
-			
+func main() {
+	serverPort := "8000"
+	if len(os.Args) == 2 {
+		serverPort = os.Args[1]
 	}
+	ln, err := net.Listen("tcp", ":"+serverPort)
+	if err != nil {
+		fmt.Printf("占用端口失败！\n")
+	}
+	for {
+		conn, err := ln.Accept()
+		//fmt.Printf("New client come in!\n")
+		if err != nil {
+			fmt.Printf("%v！\n", err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	var newClient client
+	newClient.userAddr = conn.RemoteAddr().String()
+	//fmt.Printf("New client come in!Address is %v\n", newClient.userAddr)
+
+	inputMsg := bufio.NewScanner(conn)
+	for inputMsg.Scan() {
+		if newClient.online == false {
+			newClient.userName = inputMsg.Text()
+			fmt.Printf("%v enter the room\n", newClient.userName)
+			newClient.online = true
+		} else {
+			newClient.msg = inputMsg.Text()
+			fmt.Printf("%v:%v\n", newClient.userName, newClient.msg)
+		}
+
+	}
+	fmt.Printf("%v leave the room.\n",newClient.userName)
+	defer conn.Close()
 }
